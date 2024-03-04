2782c48a9ee4b10f6eea3ff83618467a94640741
ゆうご <yuhgo090764@gmail.com>
2024-03-04 17:04:35 +0900

    shadcn/uiを導入

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/app/_component/ui/button";
+
 export default function Page() {
 	return (
 		<main>
 			<div className="m-10 text-center">
 				<h1>Hello World🚀</h1>
+				<Button>Click me</Button>
 			</div>
 		</main>
 	);
