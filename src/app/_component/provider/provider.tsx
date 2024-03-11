8a5b8b63066cab2cf287e86f1afcb8ccb046dcf5
ゆうご <yuhgo090764@gmail.com>
2024-03-11 17:12:23 +0900

    neon DBとnext/auth、prismaを使用した認証機能の実装

--- src/app/_component/provider/provider.tsx
+++ src/app/_component/provider/provider.tsx
@@ -1,12 +1,15 @@
 "use client";
 
+import { NextAuthProvider } from "@/app/_component/provider/next-auth-provider";
 import { ThemeProvider } from "@/app/_component/provider/theme-provider";
 import type { FC, PropsWithChildren } from "react";
 
 export const Provider: FC<PropsWithChildren> = ({ children }) => {
 	return (
-		<ThemeProvider attribute="class" defaultTheme="dark" enableSystem={true} disableTransitionOnChange={true}>
-			{children}
-		</ThemeProvider>
+		<NextAuthProvider>
+			<ThemeProvider attribute="class" defaultTheme="dark" enableSystem={true} disableTransitionOnChange={true}>
+				{children}
+			</ThemeProvider>
+		</NextAuthProvider>
 	);
 };
