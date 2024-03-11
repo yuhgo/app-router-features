ed456154160043f2f2b4e31e1f716f0f36ca11d4
ゆうご <yuhgo090764@gmail.com>
2024-03-11 12:48:37 +0900

    ホットキーウィジェットを実装

---
+++ src/app/_component/widget/hot-key.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { cn } from "@/_lib/shadcn/utils";
+import { type FC, useState } from "react";
+import { useHotkeys } from "react-hotkeys-hook";
+
+export const HotKey: FC = () => {
+	const [active, setActive] = useState(false);
+
+	useHotkeys("meta+u, ctrl+u", () => setActive((v) => !v), {
+		preventDefault: true,
+	});
+
+	return (
+		<kbd
+			className={cn(
+				"rounded-md border bg-muted px-2 py-1.5 font-mono font-semibold text-sm transition duration-500",
+				active
+					? "border-pink-500 bg-pink-500 text-white shadow-[0_1px_7px] shadow-pink-500"
+					: "bg-muted text-muted-foreground shadow-sm",
+			)}
+		>
+			<span className="mr-1">⌘</span>U
+		</kbd>
+	);
+};
