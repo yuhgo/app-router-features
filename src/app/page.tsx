12b026ade87b12c52ba2fee26e3f2c8fc0a2e6a2
ゆうご <yuhgo090764@gmail.com>
2024-03-12 14:52:59 +0900

    レイアウトを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import { extractYouTubeVideoId } from "@/_lib/extractYouTubeVideoId";
+import { cn } from "@/_lib/shadcn/utils";
 import { Explorer } from "@/app/_component/explorer/explorer";
-import { Button } from "@/app/_component/ui/button";
+import { AuthWidget } from "@/app/_component/widget/auth-widget";
 import { HotKeyWidget } from "@/app/_component/widget/hot-key-widget";
 import { ImageCropperWidget } from "@/app/_component/widget/image-cropper-widget";
 import { QrCodeWidget } from "@/app/_component/widget/qr-code-widget";
 import { VideoPlayerWidget } from "@/app/_component/widget/video-player-widget";
-import Link from "next/link";
+import { cva } from "class-variance-authority";
 import type { FC, ReactNode } from "react";
 
 export default function Page() {
@@ -20,61 +21,105 @@ export default function Page() {
 			<main className="flex-1">
 				<div className="grid h-fit w-fit grid-cols-2 gap-4 lg:grid-cols-6 lg:gap-5">
 					{/* YouTube カード */}
-					<div className="relative col-span-2 aspect-video lg:col-span-3 lg:row-span-2 lg:aspect-auto">
-						<div className="outline-2 outline-transparent relative size-full rounded-md outline outline-offset-4 transition-[outline] duration-500">
+					<div className="relative col-span-2 lg:col-span-3 lg:row-span-2">
+						<WidgetWrapper aspectRatio="video">
 							<VideoPlayerWidget videoId={youtubeVideoId} />
-						</div>
+						</WidgetWrapper>
 					</div>
 
-					<div className="inset-0 col-span-1 aspect-square">
+					<div className="col-span-1">
 						<WidgetWrapper>
 							<QrCodeWidget />
 						</WidgetWrapper>
 					</div>
-
+					{/* いいね */}
 					<div className="bg-red-200">1x1</div>
+					{/* 読み上げ */}
 					<div className="bg-red-200">1x1</div>
-					<div className="inset-0 col-span-1 aspect-square">
+					<div className="col-span-1">
 						<WidgetWrapper>
 							<HotKeyWidget />
 						</WidgetWrapper>
 					</div>
-					<div className="col-span-2 bg-blue-200">1x2</div>
+					{/* スター */}
 					<div className="col-span-2 bg-blue-200">1x2</div>
 
-					<div className="inset-0 col-span-1 aspect-square">
+					{/* メール送信 */}
+					<div className="col-span-2 bg-blue-200">1x2</div>
+
+					<div className="col-span-1">
 						<WidgetWrapper>
 							<ImageCropperWidget />
 						</WidgetWrapper>
 					</div>
 
+					{/* 言語切替 */}
 					<div className="bg-red-200">1x1</div>
+
+					{/* テーマ切り替え */}
 					<div className="bg-red-200">1x1</div>
+
+					{/* 3D */}
 					<div className="bg-red-200">1x1</div>
-					<div className="inset-0 col-span-2 aspect-auto">
-						<Button>
-							<Link href="/api/auth/signin">ログイン</Link>
-						</Button>
+
+					<div className="col-span-2 row-span-1 min-h-[110px]">
+						<WidgetWrapper aspectRatio="auto">
+							<AuthWidget />
+						</WidgetWrapper>
 					</div>
+
+					{/* チャート */}
+					<div className="col-span-2 row-span-1 bg-blue-200">1x2</div>
+
+					{/* 日付フォーマット */}
 					<div className="col-span-2 bg-blue-200">1x2</div>
-					<div className="col-span-2 bg-blue-200">1x2</div>
+
+					{/* リンクプレビュー */}
 					<div className="col-span-3 bg-green-200">1x3</div>
+
+					{/* 時計 */}
 					<div className="col-span-3 bg-green-200">1x3</div>
+
+					{/* マーキー(Marquee) */}
 					<div className="col-span-6 bg-orange-200">1x6</div>
-					<div className="col-span-6 bg-orange-200">1x6</div>
+
+					{/* Form */}
 					<div className="col-span-3 row-span-2 bg-violet-200">2x3</div>
+
+					{/* スライダー */}
+					<div className="col-span-3 row-span-2 bg-violet-200">2x3</div>
+
+					{/* カラーテーマ */}
+					<div className="bg-red-200">1x1</div>
 				</div>
 			</main>
 		</div>
 	);
 }
 
+const widgetWrapperVariants = cva(
+	"relative flex aspect-square size-full flex-col items-center justify-center rounded-lg border bg-widget",
+	{
+		variants: {
+			aspectRatio: {
+				square: "aspect-square",
+				video: "aspect-video",
+				auto: "aspect-auto",
+			},
+		},
+		defaultVariants: {
+			aspectRatio: "square",
+		},
+	},
+);
+
 type WidgetWrapperProps = {
+	aspectRatio?: "square" | "video" | "auto";
 	children: ReactNode;
 };
 
 export const WidgetWrapper: FC<WidgetWrapperProps> = (props) => {
-	const { children } = props;
+	const { aspectRatio, children } = props;
 
 	return (
 		// TODO: widgetのコード表示モードかつwidget選択時にoutlineを表示
@@ -82,11 +127,17 @@ export const WidgetWrapper: FC<WidgetWrapperProps> = (props) => {
 		<div className="outline-2 outline-transparent relative size-full rounded-lg outline outline-offset-4 transition-[outline] duration-500">
 			{/* TODO: widgetのコード表示モードの場合に表示 */}
 			{/* <span className="-top-2 -right-2 absolute z-10 hidden h-3 w-3 opacity-100 transition-opacity duration-500 lg:block">
-				<span className="absolute inset-0 block animate-ping rounded-full bg-sky-400" />
-				<span className="absolute inset-0 rounded-full bg-sky-500" />
+				<span className="absolute block animate-ping rounded-full bg-sky-400" />
+				<span className="absolute rounded-full bg-sky-500" />
 			</span> */}
 
-			<div className="relative inset-0 flex aspect-square size-full flex-col items-center justify-center rounded-lg border bg-red-300 bg-widget">
+			<div
+				className={cn(
+					widgetWrapperVariants({
+						aspectRatio,
+					}),
+				)}
+			>
 				{children}
 			</div>
 		</div>
